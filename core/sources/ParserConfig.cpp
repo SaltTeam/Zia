3a4d12c1ace2b182bc24c3e97774a5a44139aa53
maxencedcx <maxence.delacroix@epitech.eu>
2018-02-23 18:34:32 +0100

    parser config up to date, tested but may need some improvements or specific behaviours

--- core/sources/ParserConfig.cpp
+++ core/sources/ParserConfig.cpp
@@ -5,7 +5,11 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+
 #include "ParserConfig.hpp"
+
+using namespace zia::apipp;
+using namespace nlohmann;
 
 
 //<editor-fold>			CONSTRUCTOR AND DESTRUCTOR
@@ -20,26 +24,116 @@ ParserConfig::~ParserConfig() {
 //</editor-fold>
 
 
+//<editor-fold>			UTILS
+
+std::string cleanString(std::string value)
+{
+	if (value.at(0) == '"')
+		value.erase(0);
+	if (value.at(value.length() - 1) == '"')
+		value.erase(value.length() - 1);
+	return value;
+}
+//</editor-fold>
+
 
 //<editor-fold>			METHODS
 
-void ParserConfig::parse(std::string path)
+template<typename T>
+void ParserConfig::putValueInElem(T value, zia::apipp::ConfElem &elem, const std::string &key) const
 {
-    std::string file_content;
-    nlohmann::json parsed;
-
-    file_content = getFileContent(path);
-	if (file_content.empty())
-		return;
-	parsed = nlohmann::json::parse(file_content);
-	for (auto it : parsed["module"])
-		std::cout << it << std::endl;
+	if (elem.getType() == ConfElem::Type::Array)
+		elem.push(ConfElem().set(value));
+	else if (elem.getType() == ConfElem::Type::Map)
+		elem.set_at(key, ConfElem().set(value));
+	else
+		elem.set(value);
 }
 
-std::string ParserConfig::getFileContent(std::string path) const
+void ParserConfig::manageValue(json::value_type &value, zia::apipp::ConfElem &elem, std::string const &key) const
+{
+	if (value.is_string())
+	{
+		value = cleanString(static_cast<std::string>(value));
+		putValueInElem(static_cast<std::string>(value), elem, key);
+	}
+	else if (value.is_boolean())
+		putValueInElem(static_cast<bool>(value), elem, key);
+	else if (value.is_number_integer())
+		putValueInElem(static_cast<long long int>(value), elem, key);
+	else if (value.is_number_float())
+		putValueInElem(static_cast<double>(value), elem, key);
+}
+
+void ParserConfig::putValues(json::object_t object, ConfElem &elem, std::string const & key) const
+{
+	std::shared_ptr<ConfElem> map;
+	map = std::make_shared<ConfElem>();
+	map->set(ConfMap());
+
+	if (elem.getType() == ConfElem::Type::Array)
+		elem.push(map);
+	else if (elem.getType() == ConfElem::Type::Map)
+		elem.set_at(key, map);
+	else
+		elem = *map;
+
+	for (auto &value : object)
+		chooseCorrectFunction(value.second, *map, value.first);
+}
+
+void ParserConfig::putValues(json::array_t array, ConfElem &elem, std::string const & key) const
+{
+	std::shared_ptr<ConfElem> array_;
+	array_ = std::make_shared<ConfElem>();
+	array_->set(ConfArray());
+
+	if (elem.getType() == ConfElem::Type::Array)
+		elem.push(array_);
+	else if (elem.getType() == ConfElem::Type::Map)
+		elem.set_at(key, array_);
+	else
+		elem = *array_;
+
+	for (auto &value : array)
+		chooseCorrectFunction(value, *array_);
+}
+
+void ParserConfig::chooseCorrectFunction(json::value_type &value, zia::apipp::ConfElem &elem, const std::string &key) const
+{
+	if (value.is_null())
+	{ }
+	else if (value.is_object())
+		putValues(static_cast<json::object_t>(value), elem, key);
+	else if (value.is_array())
+		putValues(static_cast<json::array_t>(value), elem, key);
+	else
+		manageValue(value, elem, key);
+}
+
+ConfElem ParserConfig::parse(std::string const &path)
+{
+    json parsed;
+	ConfElem conf;
+
+	try {
+		parsed = json::parse(getFileContent(path));
+	}
+	catch (std::exception &e) {
+		return conf;
+	}
+
+	chooseCorrectFunction(parsed, conf);
+
+	std::cout << conf << std::endl;
+
+	return conf;
+}
+
+std::string ParserConfig::getFileContent(std::string const &path) const
 {
     std::ifstream file;
-    std::string content = "";
+    std::string content;
     std::string line;
 
     file.open(path);
@@ -51,6 +145,7 @@ std::string ParserConfig::getFileContent(std::string path) const
     }
     else
 		std::cerr << "Cannot open file '" + path + "'" << std::endl;
+
     return content;
 }
 //</editor-fold>
