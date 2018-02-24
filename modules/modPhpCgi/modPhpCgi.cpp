6565e564667b99c7a9d0208c41e1a48f33a1e678
pillon_m <maxime.pillon@epitech.eu>
2018-02-24 18:59:43 +0100

    has to be tested waiting for pipeline to be functional

--- modules/modPhpCgi/modPhpCgi.cpp
+++ modules/modPhpCgi/modPhpCgi.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <unistd.h>
 #include <exception>
+#include <wait.h>
 #include "sza-plus-plus/api/module.h"
 #include "modules/MIME_types.hpp"
 #include "modPhpCgi.hpp"
@@ -15,7 +16,7 @@ bool modPhpCgi::perform() {
 
     std::vector<std::string> aArgs;
     aArgs.emplace_back("/usr/bin/php-cgi");
-    aArgs.emplace_back("/test.php");
+    aArgs.emplace_back("");
 
     sysCline = new char*[aArgs.size() + 1];
     for (int i=0; i<aArgs.size(); i++) {
@@ -26,6 +27,8 @@ bool modPhpCgi::perform() {
 
     // Construct environment
     std::vector<std::string> aEnv;
+
+    //todo add header which has be send through the body
     aEnv.emplace_back("GATEWAY_INTERFACE=CGI/1.1");
     aEnv.emplace_back("SERVER_PROTOCOL=HTTP/1.1");
     aEnv.emplace_back("QUERY_STRING=test=querystring");
@@ -33,7 +36,6 @@ bool modPhpCgi::perform() {
     aEnv.emplace_back("REDIRECT=true");
     aEnv.emplace_back("REQUEST_METHOD=POST");
     aEnv.emplace_back("CONTENT_TYPE=application/x-www-form-urlencoded;charset=utf-8");
-    aEnv.emplace_back("SCRIPT_FILENAME=/test.php");
     aEnv.push_back("CONTENT_LENGTH="+ std::to_string(this->request->body.length()));
     sysEnv = new char*[aEnv.size() + 1];
     for (int i=0; i<aEnv.size(); i++) {
@@ -50,29 +52,62 @@ bool modPhpCgi::perform() {
     /*
      * Construct the program exec
      */
-    auto body = new char[this->request->body.size() + 1];
-    strncpy(body, this->request->body.c_str(), this->request->body.size() + 1);
-    char *echocmd[] = {"echo", body, nullptr};
     int pipeForStdIn[2];
+    int pipeForStdOut[2];
     pid_t pid;
     int res;
+    int status;
 
     pipe(pipeForStdIn);
+    pipe(pipeForStdOut);
 
     if ((pid = fork()) == -1)
-        throw "error can't do a fork";
+        throw std::runtime_error("error can't do a fork");
 
     if (pid > 0) {
-        dup2(pipeForStdIn[1], 1);
+        //parent exec
+
         close(pipeForStdIn[0]);
-        res = execvp(echocmd[0], echocmd);
+        close(pipeForStdOut[1]);
+
+        // ecrire sur l'entrée standard du child
+        dprintf(pipeForStdIn[1], "%s", this->request->body);
+
+        // lire l'output du child
+        std::string response;
+        char buf[1025];
+        ssize_t ret;
+        while ((ret = read(pipeForStdOut[0], buf, 1024)) >= 0) {
+            if (ret != 1024) {
+                buf[ret] = '\0';
+               response += buf;
+                break;
+            }
+            buf[1024] = '\0';
+           response += buf;
+        }
+        if (ret == -1)
+            throw std::runtime_error("error on read result of php-cgi action");
+
+        //todo parse response cause actual had the headers
+        this->response->body = response;
+
+        // debug
+        std::cout << "Cgi response:\n" << response << std::endl;
+        // endDbug
+
+        // wait le child
+        waitpid(pid, &status, 0);
+
     } else {
-        dup2(pipeForStdIn[0], 0);
+
+        //child exec
         close(pipeForStdIn[1]);
+        close(pipeForStdOut[0]);
+
+        dup2(pipeForStdIn[0], 0);
+        dup2(pipeForStdOut[1], 1);
+
         res = execve(sysCline[0], sysCline, sysEnv);
     }
-    close(pipeForStdIn[1]);
-
-
-    //Actually it should write on the output the body response.
 }
